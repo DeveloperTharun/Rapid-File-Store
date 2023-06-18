cc279fe324a7aa298d4c86fc791155349ad3414b
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2023-06-19 05:05:03 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-	API_ID = ""
-	API_HASH = ""
+	API_ID = 8754146
+	API_HASH = "8b56a6989f6d04f6f4fe78133ade02fd"
 	BOT_TOKEN = ""
-	BOT_USERNAME = ""
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "")
+	BOT_USERNAME = "Ultra_File_Store_Bot"
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001509431270"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "5669934860"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SkMedia:Tharunraj1828@cluster0.vbdxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = ""
+	LOG_CHANNEL = -1001509431270
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
