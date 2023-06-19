fcab77ae00a7ea633ec572082980dda5b38d87fc
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2023-06-19 05:37:21 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -51,7 +51,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
         )
         share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=Sk-Network_{str_to_b64(str(SaveMessage.id))}"
         await editable.edit(
-            f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {share_link} \n\n"
+            f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: <code>{share_link}<\code> \n\n"
             f"Just Click the link to get your files!",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
@@ -90,7 +90,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=Sk-Network_{str_to_b64(file_er_id)}"
         await editable.edit(
             "**Your File Stored in my Database!**\n\n"
-            f"Here is the Permanent Link of your file: {share_link} \n\n"
+            f"Here is the Permanent Link of your file: <code>{share_link}<\code> \n\n"
             "Just Click the link to get your file!",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
